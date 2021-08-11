300b83209cc0fdd3791c625df3554c73a8cff71b
k38kiara <k38kiara@gmail.com>
2021-08-11 21:58:47 +0800

    syntax error

--- dataset/base.py
+++ dataset/base.py
@@ -19,5 +19,5 @@ class BaseDataset(Dataset):
         self.obj_names = []
 
     def image_loader(self, image):
-        return torch.tensor((np.array(image) / 255.)
+        return torch.tensor(np.array(image) / 255.)
 
